754ea66112c7fc57826215e0ccb7d5b21cd6dec3
snandhinimca086 <84886873+snandhinimca086@users.noreply.github.com>
2021-05-27 12:19:12 +1000

    Add files via upload

---
+++ Seek/src/Demo/test.java
@@ -0,0 +1,39 @@
+package Demo;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		import java.awt.AWTException;
+		import java.sql.SQLException;
+
+		import org.openqa.selenium.By;
+		import org.openqa.selenium.WebDriver;
+		import org.openqa.selenium.chrome.ChromeDriver;
+		import org.testng.annotations.Test;
+
+		public class test1 {
+			
+			@Test(priority = 0)
+			public void NewApp() throws SQLException, ClassNotFoundException, InterruptedException, AWTException {
+				System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Java\\chromedriver.exe");
+						
+						            WebDriver driver = new ChromeDriver();
+						  driver.get("https://codility-frontend-prod.s3.amazonaws.com/media/task_static/qa_csharp_search");
+						  driver.manage().window().maximize();
+						  Thread.sleep(1000);
+						
+						 driver.findElement(By.xpath("//*[@id='search-input']")).sendKeys("Test");
+						 Thread.sleep(5000);
+						 
+						
+						//*[@id="search-button"]
+						 
+						 /*driver.findElement(By.xpath("")).click();
+						 Thread.sleep(5000);*/
+						 
+						 driver.findElement(By.xpath("//*[@id=\'search-button\']")).click();
+						 Thread.sleep(5000);
+	}
+
+}
